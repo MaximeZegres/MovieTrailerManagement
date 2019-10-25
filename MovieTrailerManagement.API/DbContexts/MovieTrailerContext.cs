8702c12408e88622ae19082aef48eddfc1860c0e
maxime.zegres <maxime.zegres@gmail.com>
2019-10-25 05:49:35 +0200

    Add project files.

---
+++ MovieTrailerManagement.API/DbContexts/MovieTrailerContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieTrailerManagement.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTrailerManagement.API.DbContexts
+{
+    public class MovieTrailerContext : DbContext
+    {
+        public MovieTrailerContext(DbContextOptions<MovieTrailerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ MovieTrailerManagement.API/Entities/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTrailerManagement.API.Entities
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public DateTimeOffset ReleaseDate {get;set;}
+
+        public string UrlImdb { get; set; }
+
+        public DateTimeOffset AddedDate { get; set; }
+
+
+
+    }
+}
