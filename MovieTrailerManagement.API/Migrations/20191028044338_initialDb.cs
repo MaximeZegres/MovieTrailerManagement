a8fef98830045cc39c13b50f9071b4cf7907e06a
maxime.zegres <maxime.zegres@gmail.com>
2019-10-28 05:46:37 +0100

    Initial EF migration

---
+++ MovieTrailerManagement.API/Migrations/20191028044338_initialDb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieTrailerManagement.API.Migrations
+{
+    public partial class initialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    ReleaseDate = table.Column<DateTimeOffset>(nullable: false),
+                    UrlImdb = table.Column<string>(nullable: true),
+                    AddedDate = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
