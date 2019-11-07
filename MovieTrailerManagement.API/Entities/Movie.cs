5a96778046950019eefc498deffc81f0011ce4ec
maxime.zegres <maxime.zegres@gmail.com>
2019-11-07 11:17:31 +0100

    Add validation on Title and ReleaseDate

--- MovieTrailerManagement.API/Entities/Movie.cs
+++ MovieTrailerManagement.API/Entities/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace MovieTrailerManagement.API.Entities
     public class Movie
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Ce champ est obligatoire.")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = "Ce champ est obligatoire.")]
         public DateTimeOffset ReleaseDate {get;set;}
 
         public string UrlImdb { get; set; }
