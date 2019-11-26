9f70042c6fb724bfac2864fc145f458b9be497db
maxime.zegres <maxime.zegres@gmail.com>
2019-11-26 05:30:42 +0100

    update primeNG

--- MovieTrailerManagementClientApp/src/app/app.module.ts
+++ MovieTrailerManagementClientApp/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './movies/movie-list.component';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import { MovieListComponent } from './movies/movie-list.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    CardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
